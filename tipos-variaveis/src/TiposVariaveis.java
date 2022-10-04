e6d469bc708338ca17d1bab045f74599b191121b
Guto Silva <carlosac_silva@yahoo.com.br>
2022-10-04 10:05:35 -0300

    Java Básico - Tipos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,18 @@
 package src;
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Olá, Java");
+        byte idade = 31;
+        short ano = 2022;
+        int cep = 21070333;
+        float pi = 3.14f;
+        double salario = 2500.0;
+        long cpf = 11122233344L;
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(pi);
+        System.out.println(salario);
+        System.out.println(cpf);
     }
 }
